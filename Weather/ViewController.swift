05a14df56b2d73e18d574d4ceff81ef6ec24fd58
Tatsiana Pasiukova <tatiana.will.read@gmail.com>
2019-07-07 17:02:34 +0300

    Commit title here

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -1,10 +1,48 @@
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var daysTableView: UITableView!
+    @IBOutlet weak var hoursCollectionView: UICollectionView!
+    @IBOutlet weak var currentCity: UILabel!
     
     let arrayOfDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sut", "Sun", "Tue", "Wed", "Thu", "Fri", "Sut", "Sun"]
+    var locationManager = CLLocationManager()
+    var urlString = "https://www.metaweather.com/api/"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocation = manager.location else { return }
+        fetchCityAndCountry(from: location) { city, country, error in
+            guard let city = city, let country = country, error == nil else { return }
+            print(city + ", " + country)
+            self.currentCity.text = city
+        }
+    }
+    
+    func updateLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self //как только меняется место положение срабатывает метод делегата и получаем новую точку
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.startUpdatingLocation() // слежение за местом положения
+        }
+    }
+    
+    func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            completion(placemarks?.first?.locality,
+                       placemarks?.first?.country,
+                       error)
+        }
+    }
+
     
 }
 
@@ -21,3 +59,16 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = hoursCollectionView.dequeueReusableCell(withReuseIdentifier: "collectionIdentifier", for: indexPath) as? HoursCollectionViewCell else { return UICollectionViewCell()}
+        cell.hoursCell.text = String(indexPath.row)
+        return cell
+    }
+    
+    
+}
